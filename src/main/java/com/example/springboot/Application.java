d094b6f35db93316b34d19f3f4ae15197489dcc3
yeollow <ljy2784437@gmail.com>
2020-08-20 00:32:42 +0900

    thymeleaf

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 //https://melonpeach.tistory.com/category/%EC%9B%B9%EA%B0%9C%EB%B0%9C/%EC%8A%A4%ED%94%84%EB%A7%81%20%EA%B2%8C%EC%8B%9C%ED%8C%90%20%EB%A7%8C%EB%93%A4%EA%B8%B0?page=4
-@SpringBootApplication
+@SpringBootApplication              //Tomcat웹 서버를 내장하고 있음
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/example/springboot/controller/UserController.java
+++ src/main/java/com/example/springboot/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.springboot.service.UserService;
 import com.example.springboot.domain.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -12,10 +13,15 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/")
-    public String hello() {
-        return "hello fucking spring";
+//    web browser에서 localhost:8080/hello가 들어오면 내장 톰캣 서버(WAS)에 의해 Spring이 Mapping하고 있는 곳으로 던져 method 실행.
+    @GetMapping("hello")
+    public String hello(Model model) {              //MVC에서 M의 Model
+        model.addAttribute("data", "hello thymeleaf!!");         //key, value
+
+        return "hello";     //resources의 templates에 hello라는 html파일과 matching됨
     }
+//    기본적으로 Controller에서 return값으로 문자를 반환하게 되면 viewResolver가 resources:templates/{return값}.html과 매핑되어 동작한다.
+//    지금은 뭔가 약간 matching되지 않은 것 같은데.. thymeleaf 의존성 주입이 안된건가..? 아님 templates 디렉토리를 임의로 만들어서 그런건가???
 
     @GetMapping("/user/{userSeq}")
     public User getUser(@PathVariable("userSeq") long userSeq) {
@@ -26,6 +32,7 @@ public class UserController {
     @PostMapping("/user")
     public void addUser(@RequestBody User user) {
 //        log.info("{}", user);
+//        println등과 같은 출려은 하지 않는다! 로그 관련 내용을 확인하지 못하기 때문
         userService.addUser(user);
     }
 
